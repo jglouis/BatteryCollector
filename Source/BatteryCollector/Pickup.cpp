9fc46f6f529f47f5462ecc308c1f921597fa0a0e
Jean-Guillaume Louis <jguillame.louis@gmail.com>
2015-11-04 20:20:30 +0100

    Creating Pickup class.

---
+++ Source/BatteryCollector/Pickup.cpp
@@ -0,0 +1,32 @@
+// hexode.xyz
+
+#include "BatteryCollector.h"
+#include "Pickup.h"
+
+
+// Sets default values
+APickup::APickup()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Create the static mesh component
+	pickupMesh = CreateAbstractDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));;
+	RootComponent = pickupMesh;
+
+}
+
+// Called when the game starts or when spawned
+void APickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APickup::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
